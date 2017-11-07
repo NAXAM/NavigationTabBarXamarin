0d7002a32fb9208abe6c7ee7c6e42ebc59561ded
Do Binh <binh@naxam.net>
2017-11-07 14:46:25 +0700

    update version 1.2.6 pre

--- NavigationTabBar/Properties/AssemblyInfo.cs
+++ NavigationTabBar/Properties/AssemblyInfo.cs
@@ -1,28 +1,30 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using Android.App;
 
-// Information about this assembly is defined by the following attributes. 
-// Change them to the values specific to your project.
-
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
 [assembly: AssemblyTitle("NavigationTabBar")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Xamarin.Android Binding Library - NavigationTabBar")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("martijnvandijk")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("NAXAM COMPANY LIMITED")]
+[assembly: AssemblyProduct("X Bindings")]
+[assembly: AssemblyCopyright("Copyright (c) 2017 NAXAM")]
+[assembly: AssemblyTrademark("NAXAM")]
 [assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
 
-// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
-// The form "{Major}.{Minor}.*" will automatically update the build and revision,
-// and "{Major}.{Minor}.{Build}.*" will update just the revision.
-
-[assembly: AssemblyVersion("1.0.0")]
-
-// The following attributes are used to specify the signing key for the assembly, 
-// if desired. See the Mono documentation for more information about signing.
-
-//[assembly: AssemblyDelaySign(false)]
-//[assembly: AssemblyKeyFile("")]
-
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.2.6.0")]
+[assembly: AssemblyFileVersion("1.2.6.0")]
--- demo/NavigationTabBarQs/MainActivity.cs
+++ demo/NavigationTabBarQs/MainActivity.cs
@@ -1,9 +1,10 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
-using Com.Gigamole.Navigationtabbar.Ntb;
 using System.Collections.Generic;
 using Android.Graphics;
+using Devlight.IO.Library.Ntb;
+using static Devlight.IO.Library.Ntb.NavigationTabBar;
 
 namespace NavigationTabBarQs
 {
@@ -23,22 +24,28 @@ namespace NavigationTabBarQs
             toolbar.IconSizeFraction = (0.2f);
             toolbar.IsTitled = true;
             toolbar.TitleSize = 24;
+            toolbar.IsBadged = true;
+            toolbar.SetBadgeGravity(BadgeGravity.Bottom);
+            toolbar.SetBadgePosition(BadgePosition.Center);
+            toolbar.BadgeBgColor = Color.Yellow; 
         }
 
         IList<NavigationTabBar.Model> GetModels()
         {
             List<NavigationTabBar.Model> models = new List<NavigationTabBar.Model>();
-            models.Add(
-                    new NavigationTabBar.Model.Builder(
+            var model = new NavigationTabBar.Model.Builder(
                             Resources.GetDrawable(Resource.Drawable.ic_first, Theme),
                             Color.Red)
                             .Title("ic_first")
-                            .Build()
-            );
+                            .BadgeTitle("Quynh Anh")
+                            .Build();
+            model.ShowBadge();
+            models.Add(model);
             models.Add(
                     new NavigationTabBar.Model.Builder(
                             Resources.GetDrawable(Resource.Drawable.ic_second, Theme),
                             Color.Blue)
+                            .BadgeTitle("Thuy Linh")
                             .Title("ic_second")
                             .Build()
             );
